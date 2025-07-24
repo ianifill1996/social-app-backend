948e4879ffa5c9ba92ac7becac88f12f0d03d3a7
Ian Ifill <ianifill1996@gmail.com>
2025-07-24 14:06:39 -0400

    added code to server.js

--- server.js
+++ server.js
@@ -10,6 +10,22 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const PROD_URL = process.env.PROD_URL;
+
+// 1
+const whitelist = ["http://localhost:3000", PROD_URL];
+// 2
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+// 3
+app.use(cors(corsOptions));
 
 app.use(cors({origin: "http://localhost:5173"}))
 app.use(express.urlencoded({ extended: true }));
